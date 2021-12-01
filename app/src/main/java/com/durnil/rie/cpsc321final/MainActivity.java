8edd974c0e73f43a685787a6d79131b54198ce18
Rie <rdurnil@zagmail.gonzaga.edu>
2021-12-01 15:39:26 -0800

    Service subclass

--- app/src/main/java/com/durnil/rie/cpsc321final/MainActivity.java
+++ app/src/main/java/com/durnil/rie/cpsc321final/MainActivity.java
@@ -89,4 +89,27 @@ public class MainActivity extends AppCompatActivity {
             startButton.setEnabled(true); //Toggling the start button
         }
     }
+
+    public class BackgroundService extends Service {
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+        }
+    }
 }
