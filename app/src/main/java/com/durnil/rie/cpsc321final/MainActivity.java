136c10bc8bc37db106afb1d8e46f07da130f849a
Rie <rdurnil@zagmail.gonzaga.edu>
2021-12-11 15:50:13 -0800

    getting location

--- app/src/main/java/com/durnil/rie/cpsc321final/MainActivity.java
+++ app/src/main/java/com/durnil/rie/cpsc321final/MainActivity.java
@@ -1,11 +1,17 @@
 package com.durnil.rie.cpsc321final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -14,6 +20,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
     /* public static final String TAG = "MainActivity";
@@ -31,11 +39,44 @@ public class MainActivity extends AppCompatActivity {
     boolean serviceBound;
     BackgroundService service; */
 
+    static final int LOCATION_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        enableMyLocation();
+    }
+
+    private void enableMyLocation() {
+        // get the user's permission to access their fine location
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            // need to request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+        }
+    }
+
+    /**
+     * Gets the result of the location permission request.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation();
+            } else {
+                Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
          /*runnable = new Runnable() {
             @Override
             public void run() {
@@ -93,8 +134,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Toast.makeText(MainActivity.this, "runServiceButton pressed", Toast.LENGTH_SHORT).show();
             }
-        });*/
-    }
+        });
+    }*/
 
     /**
      * Function to update the seconds text:
