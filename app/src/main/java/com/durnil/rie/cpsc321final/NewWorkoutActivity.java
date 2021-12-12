1ea3d4e906ded0fc5759d709122cc21cfa0308b5
Jaymin <jayminwest@gmail.com>
2021-12-11 16:03:34 -0800

    worked on endWorkoutActivity

--- app/src/main/java/com/durnil/rie/cpsc321final/NewWorkoutActivity.java
+++ app/src/main/java/com/durnil/rie/cpsc321final/NewWorkoutActivity.java
@@ -1,15 +1,37 @@
 package com.durnil.rie.cpsc321final;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.MapView;
+
 public class NewWorkoutActivity extends AppCompatActivity {
+    Button startButton;
+    MapView mapView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_workout);
+
+        startButton = findViewById(R.id.startWorkoutButton);
+        mapView = findViewById(R.id.mapViewNewWorkout);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newWorkoutIntent = new Intent(NewWorkoutActivity.this, InProgressWorkoutActivity.class);
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/durnil/rie/cpsc321final/endWorkoutActivity.java
+++ app/src/main/java/com/durnil/rie/cpsc321final/endWorkoutActivity.java
@@ -1,6 +1,54 @@
 package com.durnil.rie.cpsc321final;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.MapView;
+
+/**
+ * The endWorkoutActivity class is responsible for taking the user and map data and
+ *  entering it as a new entry into the database
+ */
 public class endWorkoutActivity extends AppCompatActivity {
+    MapView mapView;
+    Spinner typeSpinner;
+    TextView workoutName;
+    TextView timeTV;
+    TextView distanceTV;
+    TextView avgSpeedTV;
+    Button deleteButton;
+    Button saveButton;
+    WorkoutOpenHelper helper;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Setting up the views:
+        mapView = findViewById(R.id.newWorkoutMap);
+        workoutName = findViewById(R.id.workoutNameEditText);
+        timeTV = findViewById(R.id.newWorkoutTimeValueTextView);
+        distanceTV = findViewById(R.id.newWorkoutDistDataTextView);
+        avgSpeedTV = findViewById(R.id.newWorkoutSpeedDataTextView);
+        saveButton = findViewById(R.id.newWorkoutSaveButton);
+        deleteButton = findViewById(R.id.newWorkoutDeleteButton);
+        typeSpinner = findViewById(R.id.workoutTypeSpinner);
+        //Setting up the spinner:
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.types_array, android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(adapter);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Workout temp = new Workout()
+            }
+        });
+    }
 }
